79181f675872d6aa0c39537efe3b0e43bf34cba5
Hong Man Ho <ghdaksgh@gmail.com>
2021-07-23 13:59:22 +0900

    Update simulation.R

--- WF_Edge_ASSY/simulation.R
+++ WF_Edge_ASSY/simulation.R
@@ -9,40 +9,36 @@ if(!suppressMessages(require(data.table))){install.packages('data.table')}; requ
 
 setwd('C:\\Users\\mano.hong\\Desktop\\PROJECT\\21. 07. WF 구배별 조립')
 
-dat <- list()
-for(i in 1:2){
-  
-  dat[[i]] <- fread(paste0('fin', i, '.csv'))
-  
-}
-
-dat <- rbindlist(dat) %>% 
-  filter(step == 'T070256' & mask == 'F' & HB %in% c(1:6)) %>% 
-  filter(substr(run, 1, 2) == 'BH') %>% 
-  mutate(NB_L = ifelse(NB == 0, 0, 1),
-         edge = ifelse(x %in% c(1:30), 'E1', 'E2'))
+dat <- fread('fin.csv') %>% 
+  mutate(NB_L = ifelse(NB == 0, 0, 1))
 
 prime <- dat %>% filter(test == 0 & HB %in% c(1:4))
 retest <- dat %>% filter(test == 1 & HB %in% c(1:6))
 
-dat <- rbind(prime, retest)
-dat <- split(dat, dat$run)
+fin_ <- rbind(prime, retest) %>% inner_join(read.table('edge.txt', header = T))
+fin <- split(fin_, fin_$run)
+
 dat_list <- list()
-for(i in 1:length(dat)){
+for(i in 1:length(fin)){
   
-  dat_list[[i]] <- split(dat[[i]], dat[[i]]$wf)
+  dat_list[[i]] <- split(fin[[i]], fin[[i]]$wf)
   
 }
 
-
 ### Simulation ----
 
-result <- get_result(dat_list, 'edge', 3)
-result2 <- result %>% 
+result <- get_result(dat_list, 'edge', 4)
+result_ <- result %>% 
   group_by(pkg) %>% 
   summarise(NB_L = ifelse(sum(NB_L) == 0, 0, 1)) %>%
   filter(!pkg %in% c(paste0('etc_E', 1:9), 'etc_center')) %>% 
   mutate(edge = str_split_fixed(pkg, '_', 3)[, 2]) %>% 
   group_by(edge) %>% 
   summarise(n = n(),
-            YLD = 1 - mean(NB_L))
+            n_fail = sum(NB_L),
+            YLD = round(1 - mean(NB_L), 4) * 100)
+
+fin_$HB_L <- ifelse(fin_$HB %in% c(1:4), 1, 0)
+round(table(fin_$HB_L)/nrow(fin_) * 100, 2)
+round((sum(result_$n) - sum(result_$n_fail)) / sum(result_$n) * 100, 5)
+
