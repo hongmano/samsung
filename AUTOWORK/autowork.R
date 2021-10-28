850b86af5dc581729331e9d99f65e274e6ff88bf
Hong Man Ho <ghdaksgh@gmail.com>
2021-10-28 13:06:17 +0900

    Update autowork.R

--- AUTOWORK/autowork.R
+++ AUTOWORK/autowork.R
@@ -9,30 +9,22 @@ if(!suppressMessages(require(BMS))){install.packages('BMS')}; require(BMS)
 if(!suppressMessages(require(lubridate))){install.packages('lubridate')}; require(lubridate)
 if(!suppressMessages(require(ggplot2))){install.packages('ggplot2')}; require(ggplot2)
 source('C:\\Users\\mano.hong\\Desktop\\AUTOWORK\\autowork_utils.R')
-memory.limit(32624)
 
 cmd <- commandArgs()
+
 
 # lscl, folder, tPD
 
 lscl <- cmd[6]
 folder <- cmd[7]
-MSR <- cmd[8]
-
 myfolder <- paste0('C:\\Users\\mano.hong\\Desktop\\AUTOWORK\\', folder)
 setwd(myfolder)
 
 if(lscl == 'n'){
   
-  file_list <- list.files()
-  mylist <- list()
+  file_list <- list.files() %>% sort()
   
-  header <- file_list[str_detect(file_list, 'HEADER')] %>% read.header()
-  test <- file_list[str_detect(file_list, '_0|_1|_2')] %>% read.test()
-  
-  dat <- test %>% 
-    inner_join(header, by = 'lot') %>%
-    unique()
+  dat <- wrangling(file_list)
   
   fwrite(dat, 'fin.csv', row.names = F)
   print('##### Data Save Done #####')
@@ -41,33 +33,27 @@ if(lscl == 'n'){
 }else{
   
   folder_list <- list.files()
+  
   for(i in 1:length(folder_list)){
-
-    setwd(paste0(myfolder, '\\', folder_list[i]))
-    file_list <- list.files()
-    mylist <- list()
     
-    header <- file_list[str_detect(file_list, 'HEADER')] %>% read.header()
-    test <- file_list[str_detect(file_list, '_00|_01|_02')] %>% read.test()
-
-    dat <- test %>% 
-      inner_join(header, by = 'lot') %>%
-      unique()
+    setwd(paste0(myfolder, '\\', folder_list[i]))
+    file_list <- list.files() %>% sort()
+    
+    dat <- wrangling(file_list) %>% 
+      mutate(tPD = as.numeric(tPD))
     
     fwrite(dat, 'fin.csv', row.names = F)
     
-    dat_f <- dat_fin(dat)
-    
-    tPD_plot(dat_f)
-    NB_plot(dat_f)
-    byRUN_plot(dat_f)
-    tPDRUN_plot(dat_f)
-    MAPRUN_plot(dat_f)
+    tPD_plot(dat)
+    NB_plot(dat)
+    byRUN_plot(dat)
+    tPDRUN_plot(dat)
+    MAPRUN_plot(dat)
     DUTMAP(dat)
-
+    
   }
-
+  
   print('##### Data Save Done #####')
   print('##### END #####')
   
-  }
+}
