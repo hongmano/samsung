bb45aa55cb3a71bfa3e7c86fdea2698e72934034
Hong Man Ho <ghdaksgh@gmail.com>
2021-11-01 11:03:48 +0900

    Update autowork.R

--- AUTOWORK/autowork.R
+++ AUTOWORK/autowork.R
@@ -8,6 +8,7 @@ if(!suppressMessages(require(reshape2))){install.packages('reshape2')}; require(
 if(!suppressMessages(require(BMS))){install.packages('BMS')}; require(BMS)
 if(!suppressMessages(require(lubridate))){install.packages('lubridate')}; require(lubridate)
 if(!suppressMessages(require(ggplot2))){install.packages('ggplot2')}; require(ggplot2)
+if(!suppressMessages(require(xlsx))){install.packages('ggplot2')}; require(ggplot2)
 source('C:\\Users\\mano.hong\\Desktop\\AUTOWORK\\autowork_utils.R')
 
 cmd <- commandArgs()
@@ -26,7 +27,7 @@ if(lscl == 'n'){
   
   dat <- wrangling(file_list)
   
-  fwrite(dat, 'fin.csv', row.names = F)
+  write.csv(dat, 'fin.csv', row.names = F)
   print('##### Data Save Done #####')
   print('##### END #####')
   
@@ -42,7 +43,7 @@ if(lscl == 'n'){
     dat <- wrangling(file_list) %>% 
       mutate(tPD = as.numeric(tPD))
     
-    fwrite(dat, 'fin.csv', row.names = F)
+    write.csv(dat, 'fin.csv', row.names = F)
     
     tPD_plot(dat)
     NB_plot(dat)
