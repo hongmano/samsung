4231f631d2e1cf0e3360fdafbd32a9150f2d4022
Hong Man Ho <ghdaksgh@gmail.com>
2022-01-18 18:55:55 +0900

    Update Converter.R

--- EVEN2ODD/Converter.R
+++ EVEN2ODD/Converter.R
@@ -5,78 +5,82 @@ library(dplyr)
 library(stringr)
 library(BMS)
 
+### 8 IPMC ALERT !!!!!!!!!!
+
 # 2. Data Loading ---------------------------------------------------------
 
-setwd('C:/Users/mano.hong/Desktop/RProject(WH)/22. 01. EPIC Even PATN ODD로 만들기 - 작성중')
+setwd('C:/Users/mano.hong/Desktop/even/even')
 
-dat <- readLines('PDBS4FADC8N5PEE.pat') %>% 
-  str_replace_all('#define E_', '#define O_') %>% 
-  str_replace_all('-IFE A E', '-IFE A O')
 
-option <- 'odd'
+files <- list.files()
+files <- files[str_detect(files, '.pat')]
 
-# 3. Extract RD -----------------------------------------------------------
-
-# EPIC PATN START
-
-RD <- str_which(dat, ' RR ')
-
-dat_list <- list()
-for(i in 1:length(RD)){
+for(index in 1:length(files)){
   
-  dat_list[[i]] <- data.frame(code = dat[(RD[i]+1):(RD[i]+16)]) %>% 
-    mutate(line = 1:16,
-           data = regmatches(code, regexpr('R[0-1]+', code)))
+  setwd('C:/Users/mano.hong/Desktop/even/even')
+
+  dat <- readLines(files[index]) %>% 
+    str_replace_all('#define E_', '#define O_') %>% 
+    str_replace_all('-IFE A E', '-IFE A O')
   
-  even <- c(1,3,5,7,9,11,13,15)
-  odd <- even+1
+  # 3. Extract RD -----------------------------------------------------------
   
-  data <- list()
-  
-  for(j in 1:4){
+  # EPIC PATN START
+  RD[i]
+  RD <- str_which(dat, ' d[1_][1_]           RD')
+  dat_list <- list()
+  for(i in 1:length(RD)){
+
+    dat_list[[i]] <- data.frame(code = dat[(RD[i]):(RD[i]+3)]) %>% 
+      mutate(line = 1:4,
+             data = regmatches(code, regexpr('R[0-1][0-1][0-1][0-1]', code)))
     
-    data[[j]] <- dat_list[[i]]$data[(4*j-3):(4*j)]
-    data[[j]] <- paste0(data[[j]][1], data[[j]][2], data[[j]][3], data[[j]][4]) %>% 
+    even <- c(1,3,5,7,9,11,13,15)
+    odd <- even+1
+    
+    data <- paste0(dat_list[[i]]$data[1],
+                   dat_list[[i]]$data[2],
+                   dat_list[[i]]$data[3],
+                   dat_list[[i]]$data[4]) %>% 
       str_remove_all('R') %>% 
       str_split_fixed('', 16)
     
-    if(option == 'even'){
-      
-      first <- rev(data[[j]][even][5:8]) %>% as.numeric()
-      second <- rev(data[[j]][even][1:4]) %>% as.numeric()
-      
-      data[[j]] <- paste0(bin2hex(first), bin2hex(second))
-      
-    }else if(option == 'odd'){
-      
-      first <- rev(data[[j]][odd][5:8]) %>% as.numeric()
-      second <- rev(data[[j]][odd][1:4]) %>% as.numeric()
-      
-      data[[j]] <- paste0(bin2hex(first), bin2hex(second))
-      
-    }else{
-      
-      stop('Set option within "even" or "odd"')
-      
-    }
+    
+    first <- rev(data[odd][5:8]) %>% as.numeric()
+    second <- rev(data[odd][1:4]) %>% as.numeric()
+    
+    data <- paste0(bin2hex(first), bin2hex(second))
+    
+    data_location <- str_locate_all(dat_list[[i]]$code[1], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
+    substr(dat_list[[i]]$code[1], data_location+5, data_location+6) <- data
+    
+    dat[(RD[i]):(RD[i]+3)] <- dat_list[[i]]$code
+    
+    name <- files[index]
+    substr(name, nchar(name)-4, nchar(name)-4) <- 'O'
+    
+    
+    
   }
   
+  dat <- dat %>% 
+    str_replace_all('#define _PDCS[0-9A-Z]+',
+                    paste0('#define _', substr(name, 1, nchar(name)-4))) %>% 
+    str_replace_all('defined\\(E_\\)',
+                    'defined\\(O_\\)') %>%
+    str_replace_all('A.AND.E',
+                    'A.AND.O') %>% 
+    str_replace_all('//      -IFE "A E"  ',
+                    '//      -IFE "A O"  ') %>% 
+    str_replace_all('PATTERN\\([A-Z0-9]+,memory\\)',
+                    paste0('PATTERN\\(', substr(name, 1, nchar(name)-4), ',memory\\)'))
   
-  data_location <- str_locate_all(dat_list[[i]]$code[1], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
-  substr(dat_list[[i]]$code[1], data_location+5, data_location+6) <- data[[1]]
-  
-  data_location <- str_locate_all(dat_list[[i]]$code[5], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
-  substr(dat_list[[i]]$code[5], data_location+5, data_location+6) <- data[[2]]
-  
-  data_location <- str_locate_all(dat_list[[i]]$code[9], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
-  substr(dat_list[[i]]$code[9], data_location+5, data_location+6) <- data[[3]]
-  
-  data_location <- str_locate_all(dat_list[[i]]$code[13], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
-  substr(dat_list[[i]]$code[13], data_location+5, data_location+6) <- data[[4]]
   
   
-  dat[(RD[i]+1):(RD[i]+16)] <- dat_list[[i]]$code
+  setwd('../odd')
+  
+  writeLines(dat, name)
   
 }
 
-write.csv(dat, 'PDBS4FADC8N5PEO.pat', quote = F, row.names = F)
+
