36c3f73c6c8e31bc721b60bcd516e92a545236d1
Hong Man Ho <ghdaksgh@gmail.com>
2022-01-21 17:14:08 +0900

    Update Converter.R

--- EVEN2ODD/Converter.R
+++ EVEN2ODD/Converter.R
@@ -9,7 +9,7 @@ library(BMS)
 
 # 2. Data Loading ---------------------------------------------------------
 
-setwd('C:/Users/mano.hong/Desktop/even/220119/even')
+setwd('C:/Users/mano.hong/Desktop/even/220121/even')
 
 
 files <- list.files()
@@ -17,7 +17,7 @@ files <- files[str_detect(files, '.pat')]
 
 for(index in 1:length(files)){
 
-  setwd('C:/Users/mano.hong/Desktop/even/220119/even')
+  setwd('C:/Users/mano.hong/Desktop/even/220121/even')
   
   dat <- readLines(files[index]) %>% 
     str_replace_all('#define E_', '#define O_') %>% 
@@ -33,69 +33,78 @@ for(index in 1:length(files)){
 
   for(i in 1:length(RD)){
     
-    bit_list <- list()
+    MRR <- substr(regmatches(dat[RD[i]], regexpr('RD\\([0-9a-z,]+\\)', dat[RD[i]])), 9, 10)
     
-    for(bit in 1:4){
-
-      bit <- bit - 1
-      if(length(bit_list) > 0){
+    if(MRR == 'f0'){
+      
+      
+      
+    }else{
+      
+      bit_list <- list()
+      
+      for(bit in 1:4){
         
-        bit <- bit_list[[length(bit_list)]] + 1
+        bit <- bit - 1
+        if(length(bit_list) > 0){
+          
+          bit <- bit_list[[length(bit_list)]] + 1
+          
+        }
         
+        repeat{
+          
+          bit_judge <- dat[RD[i]+bit] %>% str_detect('[RDBI][0-1]+')
+          
+          if(sum(bit_judge, na.rm = T) == 1){
+            bit_list <- append(bit_list, bit)
+            break
+          }
+          bit <- bit + 1
+          msg <- paste0('PATN : ', files[index],
+                        ' RD : ', RD[i],
+                        ' DATA : ', bit, 'th')
+          print(msg)
+        }
       }
       
-      repeat{
-        
-        bit_judge <- dat[RD[i]+bit] %>% str_detect('R[0-1]+')
-        
-        if(sum(bit_judge, na.rm = T) == 1){
-          bit_list <- append(bit_list, bit)
-          break
-        }
-        bit <- bit + 1
-        msg <- paste0('PATN : ', files[index],
-                      ' RD : ', RD[i],
-                      ' DATA : ', bit, 'th')
-        print(msg)
-        }
-      }
+      dat_list[[i]] <- data.frame(code = dat[c(RD[i] + bit_list[[1]],
+                                               RD[i] + bit_list[[2]],
+                                               RD[i] + bit_list[[3]],
+                                               RD[i] + bit_list[[4]])]) %>% 
+        mutate(line = 1:4,
+               data = regmatches(code, regexpr('[RDBI][0-1][0-1][0-1][0-1]', code)))
+      
+      
+      even <- c(1,3,5,7,9,11,13,15)
+      odd <- even+1
+      
+      data <- paste0(dat_list[[i]]$data[1],
+                     dat_list[[i]]$data[2],
+                     dat_list[[i]]$data[3],
+                     dat_list[[i]]$data[4]) %>% 
+        str_remove_all('R') %>% 
+        str_split_fixed('', 16)
+      
+      
+      first <- rev(data[odd][5:8]) %>% as.numeric()
+      second <- rev(data[odd][1:4]) %>% as.numeric()
+      
+      data <- paste0(bin2hex(first), bin2hex(second))
+      
+      data_location <- str_locate_all(dat_list[[i]]$code[1], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
+      substr(dat_list[[i]]$code[1], data_location+5, data_location+6) <- data
+      
+      dat[c(RD[i] + bit_list[[1]],
+            RD[i] + bit_list[[2]],
+            RD[i] + bit_list[[3]],
+            RD[i] + bit_list[[4]])] <- dat_list[[i]]$code
+      
+    }
     
-    dat_list[[i]] <- data.frame(code = dat[c(RD[i] + bit_list[[1]],
-                                             RD[i] + bit_list[[2]],
-                                             RD[i] + bit_list[[3]],
-                                             RD[i] + bit_list[[4]])]) %>% 
-      mutate(line = 1:4,
-             data = regmatches(code, regexpr('R[0-1][0-1][0-1][0-1]', code)))
 
-        
-    even <- c(1,3,5,7,9,11,13,15)
-    odd <- even+1
-    
-    data <- paste0(dat_list[[i]]$data[1],
-                   dat_list[[i]]$data[2],
-                   dat_list[[i]]$data[3],
-                   dat_list[[i]]$data[4]) %>% 
-      str_remove_all('R') %>% 
-      str_split_fixed('', 16)
-    
-    
-    first <- rev(data[odd][5:8]) %>% as.numeric()
-    second <- rev(data[odd][1:4]) %>% as.numeric()
-    
-    data <- paste0(bin2hex(first), bin2hex(second))
-    
-    data_location <- str_locate_all(dat_list[[i]]$code[1], 'RD\\([0-9,a-z]+\\)') %>% unlist %>% min
-    substr(dat_list[[i]]$code[1], data_location+5, data_location+6) <- data
-    
-    dat[c(RD[i] + bit_list[[1]],
-          RD[i] + bit_list[[2]],
-          RD[i] + bit_list[[3]],
-          RD[i] + bit_list[[4]])] <- dat_list[[i]]$code
-    
     name <- files[index]
     substr(name, nchar(name)-4, nchar(name)-4) <- 'O'
-    
-    
     
   }
   
